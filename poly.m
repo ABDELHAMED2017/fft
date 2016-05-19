7af82cf744fa532d2d188d0805f82829e701435e
tfiers <tomas.fiers@gmail.com>
2016-05-19 15:08:37 +0200

    Correct poly-function.

--- poly.m
+++ poly.m
@@ -1,9 +1,5 @@
 function [c] = poly(a,T,n)
-%POLY Gives the coëfficients of the polynomial y(n).
-c = zeros(n+1,1);
-for graad = 1:n
-    c = c + a .* T(:,graad);
-end
+%POLY Gives the coëfficients of the polynomial y_n.
+c = a * T;
 c(1) = c(1)/2;
 end
-
