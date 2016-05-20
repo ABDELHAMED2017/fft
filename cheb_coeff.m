7d5f0139407cc55f52126529c9bda5861890abaf
tfiers <tomas.fiers@gmail.com>
2016-05-20 09:41:27 +0200

    Change indent style to match given Matlab code.

--- cheb_coeff.m
+++ cheb_coeff.m
@@ -1,13 +1,14 @@
 function [a] = cheb_coeff(f_handle,N)
 %CHEB_COEFF Returns the weights a_k to be used in the Chebyshev
 %approximatinon of f_handle
-x_n = zeros(1,N+1);
-for n = 0:N
-    x_n(n+1) = f_handle(cos(pi*n/N));
-end
 
-x = [x_n fliplr(x_n(2:N))];
-f = real(fft(x));
-a = f(1:N+1)/N;
-a(2:N) = a(2:N)*2;
+    x_n = zeros(1,N+1);
+    for n = 0:N
+        x_n(n+1) = f_handle(cos(pi*n/N));
+    end
+
+    x = [x_n fliplr(x_n(2:N))];
+    f = real(fft(x));
+    a = f(1:N+1)/N;
+    a(2:N) = a(2:N)*2;
 end
--- poly.m
+++ poly.m
@@ -1,5 +1,6 @@
 function [c] = poly(a,T,n)
-%POLY Gives the coëfficients of the polynomial y_n.
-a(1) = a(1)/2;
-c = a * T;
+%POLY Returns the coëfficients of the polynomial y_n.
+
+    a(1) = a(1)/2;
+    c = a * T;
 end
