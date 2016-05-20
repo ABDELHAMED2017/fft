70acee1da8dcd6706007c2d4b3866b45ab156f2b
tfiers <tomas.fiers@gmail.com>
2016-05-20 09:53:23 +0200

    Function signatures for add_int, mul_int, sub_int.

---
+++ add_int.m
@@ -0,0 +1,9 @@
+function [c] = add_int(a, b)
+%ADD_INT Adds two integers represented as arrays.
+%
+%   a, b, and c are arrays where each element is a digit 
+%   in a high precision representation of an integer.
+
+    
+
+end
---
+++ sub_int.m
@@ -0,0 +1,11 @@
+function [c,sign] = sub_int(a, b)
+%SUB_INT Substracts two integers represented as arrays.
+%
+%   a, b, and c are arrays where each element is a digit 
+%   in a high precision representation of an integer.
+%   sign is -1 if (the number represented by) b is larger
+%   than a, +1 if it is smaller, and 0 if they are equal.
+
+    
+
+end
