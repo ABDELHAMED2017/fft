bd04f8acf4b6554fc3bdc52c9554f149c3893252
tfiers <tomas.fiers@gmail.com>
2016-05-20 14:16:24 +0200

    More tests for add_int.

--- test_add_int.m
+++ test_add_int.m
@@ -12,13 +12,20 @@ b = 1;
 c = add_int(a,b);
 assert(isequal(c, [4 2 1]));
 
-% Simple carry
+% Neutral element for addition
+% 123 + 0 = 123
+a = [3 2 1];
+b = 0;
+c = add_int(a,b);
+assert(isequal(c, a));
+
+% Carry
 % 108 + 108 = 216
 a = [8 0 1];
 c = add_int(a,a);
 assert(isequal(c, [6 1 2]));
 
-% % Multiple carries
+% Multiple carries
 % 188 + 188 = 376
 a = [8 8 1];
 c = add_int(a,a);
@@ -29,3 +36,10 @@ assert(isequal(c, [6 7 3]));
 a = [8 8];
 c = add_int(a,a);
 assert(isequal(c, [6 7 1]));
+
+% Large number test.
+a = fliplr([9 8 9 8 4 5 6 1 6 8 4 1 6 2 1 2]);
+b = fliplr([1 2 1 6 5 8 9 8 4 8 9 4 1 6 5 1 8 9 7 6 5 3 7 6 1 2 9 7 5 9 1 7]);
+sum = fliplr([1 2 1 6 5 8 9 8 4 8 9 4 1 6 5 2 8 8 7 4 9 9 3 7 8 1 3 9 2 1 2 9]);
+c = add_int(a,b);
+assert(isequal(c, sum));
